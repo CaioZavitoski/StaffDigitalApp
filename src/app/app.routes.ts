3b942d1556aab86f187436dfb84dc724a5e747c3
Caio Henrique Zavitoski <chz55@hotmail.com>
2020-01-08 09:58:18 -0300

    Menu esquerdo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,19 @@ import { ProfileSelectComponent } from './components/busca/profile-select/profil
 import { SignupComponent } from './components/signup/signup.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 import { BuscaComponent } from './components/busca/busca.component';
-import { LoginComponent } from "./components/login/login.component";
-import { EmailConfirmComponent } from "./components/email-confirm/email-confirm.component";
-import { Routes } from "@angular/router";
+import { LoginComponent } from './components/login/login.component';
+import { EmailConfirmComponent } from './components/email-confirm/email-confirm.component';
+import { Routes } from '@angular/router';
 import { SignupCompleteComponent } from './components/signup/signup-complete/signup-complete.component';
 
 export const routes: Routes = [
-  { path: "", component: LoginComponent },
-  { path: "email-confirm", component: EmailConfirmComponent },
-  { path: "login", component: EmailConfirmComponent },
-  { path: "busca", component: BuscaComponent },
-  { path: "edit-profile", component: EditProfileComponent },
-  { path: "signup", component: SignupComponent },
-  { path: "signup-complete", component: SignupCompleteComponent },
-  { path: "profile-select", component: ProfileSelectComponent },
-  { path: "forgot-password", component: ForgotPasswordComponent }
+  { path: '', component: LoginComponent },
+  { path: 'email-confirm', component: EmailConfirmComponent },
+  { path: 'login', component: EmailConfirmComponent },
+  { path: 'busca', component: BuscaComponent },
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'signup-complete', component: SignupCompleteComponent },
+  { path: 'profile-select', component: ProfileSelectComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent }
 ];
